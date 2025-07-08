6bd9d22a609948e8909a037242f14647d26ebfd2
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-07-08 23:05:36 +0700

    Scanning the whole document

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Main {
+public class SearchHANU {
+    public static void main(String[] args) {
+        String fileName = "Test.txt"; // Tên file
+        StringBuilder content = new StringBuilder();
 
-}
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append(" "); // Đọc từng dòng và nối vào chuỗi tổng
+            }}}}
