1742aa68915cf8ecaee15d881a37c8406ce3f4b8
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-08-03 23:23:10 +0700

    nhập số tính tổng bằng for

--- src/stanFord/CauTrucVonglap.java
+++ src/stanFord/CauTrucVonglap.java
@@ -1,7 +1,10 @@
 package stanFord;
 
+import java.util.Scanner;
+
 public class CauTrucVonglap {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int i =0 ;
         while (i<10){
             i++;
@@ -11,5 +14,16 @@ public class CauTrucVonglap {
         for(int j = 80 ; j< 90 ; j +=2){
             System.out.println(j);
         }
+
+        // bài 4 nhập số nguyên dương N tính
+        System.out.println("Nhập số nguyên dương vào: ");
+        int N= 0;
+        int sum = 0;
+        N = sc.nextInt();
+
+        for (int  k =0 ; k < N ; k ++){
+            sum = sum +k;
+        }
+        System.out.println("S = " + sum /N);
     }
 }
