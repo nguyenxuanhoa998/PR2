694e4670ed23f0e212cd7fbdba0026d203954a14
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-08-03 23:09:16 +0700

    vòng lặp while

---
+++ src/stanFord/CauTrucVonglap.java
@@ -0,0 +1,12 @@
+package stanFord;
+
+public class CauTrucVonglap {
+    public static void main(String[] args) {
+        int i =0 ;
+        while (i<10){
+            i++;
+            System.out.println("hôm nay nanwgg đẹp " + i);
+        }
+        System.out.println();
+    }
+}
