4df6f53421ae395d60b8f6cd58cb7d7a4ee7da85
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-08-03 23:14:54 +0700

    vòng lặp for

--- src/stanFord/CauTrucVonglap.java
+++ src/stanFord/CauTrucVonglap.java
@@ -8,5 +8,8 @@ public class CauTrucVonglap {
             System.out.println("hôm nay nanwgg đẹp " + i);
         }
         System.out.println();
+        for(int j = 80 ; j< 90 ; j +=2){
+            System.out.println(j);
+        }
     }
 }
