f80209bf5e032dc8f1b78dd19a8dab0d98b29de7
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-08-07 22:32:14 +0700

    Mảng

---
+++ src/StanFor_Mang/baiTapVeMang.java
@@ -0,0 +1,40 @@
+package StanFor_Mang;
+
+import java.util.Scanner;
+
+public class baiTapVeMang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Ví dụ 1: Khai báo mảng số nguyên gồm 3 phần tử và gán giá trị
+        int[] a = new int[3];
+        a[0] = 2;
+        a[1] = 3;
+        a[2] = 4;
+
+        System.out.println("Các phần tử trong mảng a:");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+
+        // Nhập số phần tử cho mảng thực
+        System.out.print("\nNhập số phần tử cho mảng thực: ");
+        int n = sc.nextInt();
+
+        // Khai báo mảng thực a2
+        float[] a2 = new float[n];
+
+        System.out.println("Nhập các phần tử cho mảng:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("a2[" + i + "] = ");
+            a2[i] = sc.nextFloat();
+        }
+
+        System.out.printf("\nCác phần tử của mảng thực gồm %d phần tử:\n", n);
+        for (int i = 0; i < a2.length; i++) {
+            System.out.println("a2[" + i + "] = " + a2[i]);
+        }
+
+        sc.close(); // đóng scanner để tránh warning
+    }
+}
