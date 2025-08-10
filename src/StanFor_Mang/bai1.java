715b7648a2f365d0c24d68a0b9d39dc60fe3fce2
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-08-10 23:18:45 +0700

    bài 1 tạo mảngg và nahapaj mảng

---
+++ src/StanFor_Mang/bai1.java
@@ -0,0 +1,16 @@
+package StanFor_Mang;
+
+import java.util.Scanner;
+
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int capacity;
+        System.out.print("Nhập số lượng của các thành phần vào mảng: ");
+        capacity =sc.nextInt();
+        int a [] = new int[capacity];
+        for(int i =0 ; i < a.length; i ++){
+            System.out.print(" nhập phần tử thứ")
+        }
+    }
+}
