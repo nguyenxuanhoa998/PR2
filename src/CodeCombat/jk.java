dabc5036bb5f1646960537eb5fd0b5e3b682a6ae
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-08-10 23:30:27 +0700

    vânc ko chạy dưuojc main what

---
+++ src/CodeCombat/jk.java
@@ -0,0 +1,28 @@
+package CodeCombat;
+
+import java.util.Scanner;
+
+public class jk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Nhập số lượng phần tử
+        System.out.print("Nhập số lượng phần tử: ");
+        int n = sc.nextInt();
+
+        // Khởi tạo mảng
+        int[] arr = new int[n];
+
+        // Nhập từng phần tử
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nhập phần tử thứ " + (i+1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        // In ra mảng
+        System.out.print("Các phần tử đã nhập: ");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
