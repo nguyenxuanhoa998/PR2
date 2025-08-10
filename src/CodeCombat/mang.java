cb828388dd25c1ceddcfef1f508bb42e38d9edd7
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-08-10 23:25:36 +0700

    ủa sao ko nhập dưuojc số

---
+++ src/CodeCombat/mang.java
@@ -0,0 +1,24 @@
+package CodeCombat;
+import java.util.*;
+public class mang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        int capacity = sc.nextInt();
+
+        int[] a = new int[capacity];
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("Nhập phần tử thứ " + (i + 1) + ": ");
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("\nMảng vừa nhập là:");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        sc.close();
+    }
+}
